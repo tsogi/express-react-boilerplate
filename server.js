73cf40df7b3e43489e83ab443d573fef9b4bc6b1
nikoloz93 <tsogiaidze@yahoo.com>
2019-07-13 02:12:34 +0400

    Minified server.js

--- server.js
+++ server.js
@@ -1,20 +1,5 @@
 const express = require("express");
-const Sequelize = require('sequelize');
 const initRoutes = require("./routes");
-
-const sequelize = new Sequelize('evaluator', 'root', 'nikoloz93', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
-
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully...');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
 
 const app = express();
 
