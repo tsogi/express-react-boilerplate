19cfccad7ff3016c8991e3ddf96fafa4b6d95a5d
nikoloz93 <tsogiaidze@yahoo.com>
2019-08-05 00:20:28 +0400

    Removed username. Helps #9

--- front/src/redux/reducers/user.js
+++ front/src/redux/reducers/user.js
@@ -1,8 +1,7 @@
 var store = require('store');
 
-store.set("user", { username: undefined, email: undefined, token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRzb2dpYWlkemU4QHlhaG9vLmNvbSIsImlhdCI6MTU2NDk0ODIzOH0.n-rxuHp-rjWov9JzlIdoS8CArPzI6On57hCdbdCay5E" });
-let user = store.get("user") && store.get("user").token ? store.get("user") : { username: undefined, email: undefined, token: "" };
-console.log("user", user);
+store.set("user", { email: undefined, token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRzb2dpYWlkemU4QHlhaG9vLmNvbSIsImlhdCI6MTU2NDk0ODIzOH0.n-rxuHp-rjWov9JzlIdoS8CArPzI6On57hCdbdCay5E" });
+let user = store.get("user") && store.get("user").token ? store.get("user") : { email: undefined, token: "" };
 
 const User = (state = user, action) => {
 	switch(action.type) {
